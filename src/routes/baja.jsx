45907146c494042658bc6625640903150883c166
Jordi <jordiigarciatorres@gmail.com>
2024-10-17 19:20:03 +0200

    second commit

--- src/routes/baja.jsx
+++ src/routes/baja.jsx
@@ -16,10 +16,10 @@ const Baja = () => {
     <div>    
       <div className='header'>
         <ul className='navbar'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/alta">Alta</Link></li>
-          <li><Link to="/baja">Baja</Link></li>
-          <li><Link to="/consulta">Consulta</Link></li>
+          <li><Link to="/" className='link'>Home</Link></li>
+          <li><Link to="/alta" className='link'>Alta</Link></li>
+          <li><Link to="/baja" className='link'>Baja</Link></li>
+          <li><Link to="/consulta" className='link'>Consulta</Link></li>
         </ul>
       </div>  
       <div>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,10 +4,10 @@ const Home = () => (
         <div>    
           <div className='header'>
             <ul className='navbar'>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/alta">Alta</Link></li>
-              <li><Link to="/baja">Baja</Link></li>
-              <li><Link to="/consulta">Consulta</Link></li>
+              <li><Link to="/" className='link'>Home</Link></li>
+              <li><Link to="/alta" className='link'>Alta</Link></li>
+              <li><Link to="/baja" className='link'>Baja</Link></li>
+              <li><Link to="/consulta" className='link'>Consulta</Link></li>
             </ul>
           </div>  
         </div>
