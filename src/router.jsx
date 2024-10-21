978d9c90aa6e564b37eeae0441a210b8a4256463
Jordi <jordiigarciatorres@gmail.com>
2024-10-21 15:59:24 +0200

    vercel prod

--- src/router.jsx
+++ src/router.jsx
@@ -5,7 +5,7 @@ import Baja from './routes/baja';
 import Consulta from './routes/consulta';
 
 const Router = () => (
-    <BrowserRouter basename='/frontspring'>
+    <BrowserRouter>
         <Routes>   
             <Route index element={<Home/>} />
             <Route path="/alta" element={<Alta/>} />
